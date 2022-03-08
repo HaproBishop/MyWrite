a9ff2fdfea1fbbb1ad9062c973d790ffe79b09cd
Hapro Bishop <acfowo45@gmail.com>
2022-03-08 18:00:06 +0300

    Выполнены коррекции жестов (все из-за глупых биндов на типичные клавиши)

--- TextCommands/ScaleCommands.cs
+++ TextCommands/ScaleCommands.cs
@@ -8,17 +8,17 @@ namespace TextCommands
             {
                 InputGestureCollection inputScalePlus = new InputGestureCollection()
             {
-                new KeyGesture(Key.OemPlus, ModifierKeys.Control, "Ctrl+Плюс(+)")
+                new KeyGesture(Key.OemPlus, ModifierKeys.Alt, "Alt+Плюс(+)")
             };
                 CommandScalePlus = new RoutedCommand("CommandScalePlus", typeof(ScaleCommands), inputScalePlus);
                 InputGestureCollection inputScaleMinus = new InputGestureCollection()
             {
-                new KeyGesture(Key.OemMinus, ModifierKeys.Control, "Ctrl+Минус(-)")
+                new KeyGesture(Key.OemMinus, ModifierKeys.Alt, "Alt+Минус(-)")
             };
                 CommandScaleMinus = new RoutedCommand("CommandScaleMinus", typeof(ScaleCommands), inputScaleMinus);
                 InputGestureCollection inputScaleDefault = new InputGestureCollection()
             {
-                new KeyGesture(Key.D9|Key.D0, ModifierKeys.Control, "Ctrl+()")
+                new KeyGesture(Key.D9|Key.D0, ModifierKeys.Alt, "Alt+()")
             };
                 CommandScaleDefault = new RoutedCommand("CommandDefaultScale", typeof(ScaleCommands), inputScaleDefault);
             }
---
+++ TextCommands/WinCommands.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+
+namespace TextCommands
+{
+    public class WinCommands
+    {
+        static WinCommands()
+        {
+            InputGestureCollection inputNewWindow = new InputGestureCollection()
+            {
+                new KeyGesture(Key.N, ModifierKeys.Shift|ModifierKeys.Alt, "Shift+Alt+N")
+            };
+            NewWindow = new RoutedCommand("NewWindow", typeof(WinCommands), inputNewWindow);
+        }
+        public static RoutedCommand NewWindow { get; set; }
+    }
+}
